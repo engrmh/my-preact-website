8d45693dd1d21c99d9213dd7cb886747d60924ed
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-09-13 23:41:29 +0330

    feat: portfolio

--- src/Components/ProjectBox/ProjectBox.jsx
+++ src/Components/ProjectBox/ProjectBox.jsx
@@ -1,7 +1,35 @@
+import { Button, Card, Col } from "react-bootstrap";
+import "./ProjectBox.css";
 
-
-export default function ProjectBox() {
-    return (
-        <div>ProjectBox</div>
-    )
+export default function ProjectBox({
+  title = "title",
+  getClass,
+  link = "/portfolio",
+  image = "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+}) {
+  return (
+    <Col xs={12} md={4} lg={4} className={getClass}>
+      <div class="p-4 d-flex justify-content-center align-items-center projectBox">
+        <Card style={{ width: "18rem" }} className="px-1">
+          <div class="projectBoxImgBox rounded">
+            <Card.Img
+              variant="top"
+              className="rounded projectBoxImg"
+              src={image}
+            />
+          </div>
+          <Card.Body>
+            <Card.Title className="text-center">{title}</Card.Title>
+            <a
+              href={link}
+              className="text-decoration-none btn btn-dark d-flex justify-content-center align-items-center"
+              style={{ marginTop: "1rem" }}
+            >
+              Show More
+            </a>
+          </Card.Body>
+        </Card>
+      </div>
+    </Col>
+  );
 }
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,25 +1,26 @@
-import './Portfolio.css'
+import "./Portfolio.css";
 import NavBar from "../../Components/Navbar/Navbar.jsx";
-import {Col, Row} from "react-bootstrap";
-import LocationOnIcon from "@mui/icons-material/LocationOn.js";
-import FullScreen from "../../Components/Footer/FullScreen.jsx";
+import { Col, Container, Row } from "react-bootstrap";
+import ProjectBox from "../../Components/ProjectBox/ProjectBox.jsx";
+import { useEffect } from "preact/hooks";
 
 export default function Portfolio() {
-    return (
-        <div className='portfolioContainer vh-100'>
-            <div class="">
-                <NavBar active='portfolio'/>
-            </div>
-            <div class=""></div>
-            <div className='container position-fixed bottom-0 start-0 end-0'>
-                <div className="d-flex gap-3 pb-2">
-                    <h5 className="text-white d-flex align-items-center">
-                        <LocationOnIcon className='fs-3 me-2'/>
-                        Sari, Mazandran
-                    </h5>
-                    <FullScreen/>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="portfolioContainer">
+      <div class="">
+        <NavBar active="portfolio" />
+      </div>
+      <Container class="mb-2">
+        <h3 className="text-white text-center my-5 fw-bold fst-italic">
+          The projects I did or participated in
+        </h3>
+        <Row>
+          <ProjectBox />
+          <ProjectBox />
+          <ProjectBox />
+          <ProjectBox />
+        </Row>
+      </Container>
+    </div>
+  );
 }
