100e895eb323c49198600e0a3f56c93cfe9d0ebb
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-09-13 09:16:37 +0330

    feat: updating routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,47 @@
-import Router from 'preact-router';
+import Router, {getCurrentUrl, useRouter} from 'preact-router';
 import './app.css'
 import Home from "./Pages/Home/Home.jsx";
 import About from "./Pages/About/About.jsx";
 import Page404 from "./Pages/Page404/Page404.jsx";
 import Resume from "./Pages/Resume/Resume.jsx";
+import CurrentProject from "./Pages/CurrentProject/CurrentProject.jsx";
+import Portfolio from "./Pages/Portfolio/Portfolio.jsx";
+import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
+import Projects from "./Pages/Dashboard/Projects/Projects.jsx";
 
 
 
 export function App() {
+    const location = getCurrentUrl()
+
+
+    // const currentProject = useRouter()
+    // console.log(currentProject[0].matches.current)
 
   return (
     <>
-      <Router>
-        <Home path="/" />
-        <About path="/about" />
-        <Resume path="/resume" />
-        {/*<Resume path="/portfolio" />*/}
-        <Page404 path="/*" default />
-      </Router>
+        {
+            location.includes('dashboard') ? (
+                <>
+                    <Router>
+                        <Dashboard path='/dashboard'/>
+                        <Projects path='/dashboard/projects'/>
+                        <Page404 path="/*" default />
+                    </Router>
+                </>
+            ) : (
+                <>
+                    <Router>
+                        <Home path="/" />
+                        <About path="/about" />
+                        <Resume path="/resume" />
+                        <Portfolio path="/portfolio" />
+                        <CurrentProject path="/portfolio/:current" />
+                        <Page404 path="/*" default />
+                    </Router>
+                </>
+            )
+        }
     </>
   )
 }
