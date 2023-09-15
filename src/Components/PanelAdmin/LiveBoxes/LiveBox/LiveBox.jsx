f60b0c56e104a0ebef271d86726a169eee60c8ff
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-09-15 21:55:30 +0330

    update: Live Box

--- src/Components/PanelAdmin/LiveBoxes/LiveBox/LiveBox.jsx
+++ src/Components/PanelAdmin/LiveBoxes/LiveBox/LiveBox.jsx
@@ -1,9 +1,19 @@
 import { Col } from "react-bootstrap";
 import "./LiveBox.css";
-export default function LiveBox({ title, count, classData, children }) {
+export default function LiveBox({
+  link,
+  title,
+  count,
+  containerClass,
+  classData,
+  children,
+}) {
   return (
     <Col xs={12} md={3} lg={3} className="mt-4">
-      <div class="setShadow bg-white p-2 d-flex justify-content-between align-items-center rounded">
+      <a
+        href={link}
+        class={`setShadow liveBox bg-white p-2 d-flex justify-content-between align-items-center rounded text-decoration-none text-black ${containerClass}`}
+      >
         <div
           class={`d-flex align-items-center justify-content-center p-3 rounded-3 liveBoxIcon text-white ${classData}`}
         >
@@ -13,7 +23,7 @@ export default function LiveBox({ title, count, classData, children }) {
           <h5 class="">{title}</h5>
           <h6 class="text-end">{count}</h6>
         </div>
-      </div>
+      </a>
     </Col>
   );
 }
--- src/Components/PanelAdmin/LiveBoxes/LiveBoxes.jsx
+++ src/Components/PanelAdmin/LiveBoxes/LiveBoxes.jsx
@@ -4,21 +4,63 @@ import HandymanIcon from "@mui/icons-material/Handyman";
 import GroupIcon from "@mui/icons-material/Group";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import TaskIcon from "@mui/icons-material/Task";
+import { useEffect } from "preact/hooks";
+import { gsap } from "gsap";
 
 export default function LiveBoxes() {
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        defaults: {
+          ease: "power4.in",
+        },
+      });
+      tl.to(".liveBoxGsap", {
+        y: 0,
+        autoAlpha: 1,
+        duration: 0.5,
+        stagger: 0.3,
+      });
+    });
+    // return ctx.revert()
+  }, []);
   return (
     <>
       <Row className="p-2">
-        <LiveBox title="Projects" count="1" classData="customBlack">
+        <LiveBox
+          link="/dashboard/projects"
+          title="Projects"
+          count="1"
+          containerClass="liveBoxGsap"
+          classData="customBlack"
+        >
           <HandymanIcon />
         </LiveBox>
-        <LiveBox title="Users" count="1" classData="customRed">
+        <LiveBox
+          link="/dashboard/users"
+          title="Users"
+          count="1"
+          containerClass="liveBoxGsap"
+          classData="customRed"
+        >
           <GroupIcon />
         </LiveBox>
-        <LiveBox title="Task" count="1" classData="customGreen">
+        <LiveBox
+          link="/dashboard/tasks"
+          title="Tasks"
+          count="1"
+          containerClass="liveBoxGsap"
+          classData="customGreen"
+        >
           <TaskIcon />
         </LiveBox>
-        <LiveBox title="Notifications" count="1" classData="customBlue">
+        <LiveBox
+          link="/dashboard/notifs"
+          title="Notifications"
+          count="1"
+          containerClass="liveBoxGsap"
+          classData="customBlue"
+        >
           <NotificationsIcon />
         </LiveBox>
       </Row>
