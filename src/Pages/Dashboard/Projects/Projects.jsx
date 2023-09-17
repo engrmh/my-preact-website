5c20da7896ea1e95613562a62df0f242370c2a47
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-09-17 13:38:19 +0330

    feat: projects page in dashboard

--- src/Pages/Dashboard/Projects/Projects.jsx
+++ src/Pages/Dashboard/Projects/Projects.jsx
@@ -1,3 +1,97 @@
+import { Alert, Button, Col, Row } from "react-bootstrap";
+import { DataGrid } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "name",
+      headerName: "Name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "customer",
+      headerName: "Customer",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "salary",
+      headerName: "Salary",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "projectType",
+      headerName: "Project Type",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "creator",
+      headerName: "Creator",
+      // description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      // valueGetter: (params) =>
+      //   `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+
+  // const rows = [
+  //   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  //   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  //   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  //   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  //   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  //   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  //   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  //   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  //   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  // ];
+
+  const rows = [];
+
+  return (
+    <Row className="mt-4">
+      <Col xs={12} md={12} lg={12}>
+        <div class="">
+          <div class="p-3 d-flex justify-content-between align-items-center setShadow rounded bg-white mb-3">
+            <span className="fs-5">Projects</span>
+            <Button className="p-2 customBlue customBlueHover border-0">
+              Add New Project
+            </Button>
+          </div>
+          <div class="p-3 setShadow rounded bg-white">
+            {rows.length === 0 ? (
+              <Alert variant="warning" className="text-center">
+                No Project Found. Please Add New Project
+              </Alert>
+            ) : (
+              <Box sx={{ height: 400, width: "100%" }}>
+                <DataGrid
+                  rows={rows}
+                  columns={columns}
+                  initialState={{
+                    pagination: {
+                      paginationModel: {
+                        pageSize: 6,
+                      },
+                    },
+                  }}
+                  pageSizeOptions={[6]}
+                  checkboxSelection
+                  disableRowSelectionOnClick
+                />
+              </Box>
+            )}
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
 }
