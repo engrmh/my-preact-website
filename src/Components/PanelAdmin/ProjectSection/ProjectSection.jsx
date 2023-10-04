098fa002946ec06487cc5dac565cc6678641b29f
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-10-04 21:13:31 +0330

    feat: tasks redux

--- src/Components/PanelAdmin/ProjectSection/ProjectSection.jsx
+++ src/Components/PanelAdmin/ProjectSection/ProjectSection.jsx
@@ -1,14 +1,11 @@
 import { Button, Col, Table } from "react-bootstrap";
-import AddIcon from "@mui/icons-material/Add.js";
-import DoneIcon from "@mui/icons-material/Done.js";
-import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye.js";
 
 export default function ProjectSection() {
   return (
     <Col xs={12} md={8} lg={8} className="mt-4 mb-4 mb-lg-0">
       <div className={`setShadow bg-white p-3 rounded`}>
         <div className="mb-3 d-flex justify-content-between align-items-center">
-          <span className="">Active Tasks</span>
+          <span className="">Active Users</span>
           {/*<Button className="p-0 customBlue">*/}
           {/*    <AddIcon />*/}
           {/*</Button>*/}
---
+++ src/Redux/Stores/Tasks.jsx
@@ -0,0 +1,49 @@
+const addNewTask = "ADD_NEW_TASK";
+const removeTask = "REMOVE_TASK";
+const completeTask = "COMPLETE_TASK";
+
+const taskReducer = (state = [], action) => {
+  switch (action.type) {
+    case addNewTask:
+      return [...state, action.payload];
+    case removeTask:
+      return [...state].filter((task) => task.id !== action.id);
+    case completeTask: {
+      let newTask = [...state];
+      newTask.some((task) => {
+        if (task.id === action.id) {
+          task.isDone = !task.isDone;
+        }
+      });
+      return newTask;
+    }
+    default:
+      state;
+  }
+};
+
+export const addNewTaskAction = (title) => {
+  return {
+    type: addNewTask,
+    payload: {
+      id: crypto.randomUUID(),
+      title,
+      isDone: false,
+    },
+  };
+};
+
+export const removeTaskAction = (id) => {
+  return {
+    type: removeTask,
+    id,
+  };
+};
+export const completeTaskAction = (id) => {
+  return {
+    type: completeTask,
+    id,
+  };
+};
+
+export default taskReducer;
